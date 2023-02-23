db049aab6e61175c686b9940724a78ba94b47a27
Muthoni77 <gloriamuthoni25@gmail.com>
2023-02-23 00:01:29 -0800

    fixed ClassBasedComponents

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ClassBasedComponents from './components/ClassBasedComponents';
+function App() {
+  return (
+    <div className="App">
+     
+<ClassBasedComponents/>
+<hi>hello</hi>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ClassBasedComponents.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+
+
+class ClassBasedComponents extends Component{
+    render(){
+        return(
+            <div> Class based components example</div>
+        )
+    }
+
+    
+};
+export default ClassBasedComponents;
