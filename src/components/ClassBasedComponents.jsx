754d0f67f71488a5f752254c617a352ec32063bd
Muthoni77 <gloriamuthoni25@gmail.com>
2023-02-27 12:34:35 +0300

    react form

--- src/components/ClassBasedComponents.jsx
+++ src/components/ClassBasedComponents.jsx
@@ -1,58 +1,80 @@
 import React, { Component } from "react";
+import { render } from "react-dom";
 
-class ClassBasedComponents extends Component{
+class ClassBasedComponents extends Component {
 
-state = {
-    //write all state properties inside of this
-    count : 0, //initial value of count property
-    flag : false
-}
-handleClick =()=> {
-    const {count}=this.state;
-    //set state method
-    this.setState({
-        count : count + 1
-    },()=>{
-        console.log(this.state);
+    state = {
+        FormData: {
+            name: "",
+            age: 0
+        },
+        finalFormData: {}
+    };
 
-    })
+    handleNameChange = (event) => {
+
+        const { value } = event.target;
+        this.setState(prevState => ({
+            FormData: {
+                ...prevState.FormData,
+                name: value
+            }
+        }))
+    }
+    handleAgeChange = (event) => {
+        const { value } = event.target;
+        this.setState(prevState => ({
+            FormData: {
+                ...prevState.FormData,
+                age: value
+            }
+        }))
+    }
+    handleSubmit = (event) => {
+        event.preventDefault()
+        const { FormData } = this.state
+        this.setState(prevState => ({
+            ...prevState,
+            finalFormData: {
+                ...prevState.finalFormData,
+                ...FormData
+            },
+FormData: {
+    name: "",
+    age: 0
 }
-componentDidMount(){
-    console.log("component is mounted");
-}
-componentDidUpdate(prevProps , prevState){
-    console.log(prevState , this.state);
-    if( prevState && prevState.count !== this.state.count && this.state.count === 10)
-    {
-        this.setState({
-           flag: true 
-        })
+        }))
+
     }
 
-}
-componentWillUnmount(){
-    
-}
-
-    render(){
+    render() {
         console.log(this.state);
-        return(
-            
-             <div>
-                 <button onClick={this.handleClick}>Click</button>
-                 {
-                this.state.count === 5 && "count is 5"
-             }
+        return (
+            <div>
 
-             {
-                this.state.flag  && <p> flag is true </p>
-             }
-             </div>   
-            
-            
-        )
+                <form onSubmit={this.handleSubmit} >
+                    <input name="name" placeholder="name" type={"text"} value={this.state.FormData.name} onChange={this.handleNameChange} />
+                    <input name="age" placeholder="age" type={"number"} value={this.state.FormData.age} onChange={this.handleAgeChange} />
+
+                    <button type="submit" aria-label="submit">Submit</button>
+                </form>
+
+                {
+                    this.state.finalFormData && Object.keys(this.state.finalFormData).length > 0 && (
+                        <p>
+                            final data is {this.state.finalFormData.name} and {this.state.finalFormData.age}
+                        </p>
+                    )
+                }
+
+
+
+            </div>
+
+        );
     }
 
-};
+
+}
 
 export default ClassBasedComponents;
