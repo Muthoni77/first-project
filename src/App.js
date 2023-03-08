80909420f787ca1ee3214d630d4e79a95709d3fc
Muthoni77 <gloriamuthoni25@gmail.com>
2023-03-09 00:13:28 +0300

    error boundaries

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import { createContext } from 'react';
 import './App.css';
 import FunctionalBasedComponent from './components/FunctionalBasedComponent';
+import ErrorBoundaries from './ErrorBoundaries';
 import Login from './pages/login';
 export const Context = createContext("light");
 console.log(Context);
 
 function App() {
   return (
-   <Context.Provider value='green'>
-     <div className="App">
-    
-    { <FunctionalBasedComponent/> }
-    {/* <Login/> */}
-    
-         
-        </div>
-   </Context.Provider>
+    //  <Context.Provider value='green'>
+    //    <div className="App">
+
+    //   { <FunctionalBasedComponent/> }
+    //   {/* <Login/> */}
+
+    //       </div>
+    //  </Context.Provider>
+    <ErrorBoundaries>
+      <FunctionalBasedComponent />
+    </ErrorBoundaries>
   );
 }
 
